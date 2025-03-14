f9f0937babb44cdf76ef4f79d8c4516ae8d6a65a
Angelika Brown <angelikpbrown@gmail.com>
2025-03-14 15:29:41 -0400

    Anagram Sam

--- anagram.py
+++ anagram.py
@@ -2,5 +2,16 @@ def is_anagram(first_string: str, second_string: str) -> bool:
     """
     Given two strings, this functions determines if they are an anagram of one another.
     """
-    pass  # remove pass statement and implement me
+    #checking if strings are the same length
+    if len(first_string) == len (second_string):
+        #sort the strings if same length
+        str1 = sorted(first_string)
+        str2 = sorted(second_string)
+        if str1 == str2:
+            return True
+        else:
+            False
+
+
+
 
